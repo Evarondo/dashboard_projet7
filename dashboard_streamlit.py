df7a9bdf31bad7d0ced000c69ea500d572ed83b7
Evarondo <eva-rondeau@orange.fr>
2023-07-09 18:30:33 +0200

    initialize git projet dashboard

---
+++ dashboard_streamlit.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[45]:
+
+
+import matplotlib.pyplot as plt
+import shap
+import streamlit as st
+import requests
+from PIL import Image
+from data_processing import data_prob, shap_values, data_shap_scaled, explainer, data_shap, optimal_threshold, jauge, distri_features, bivarié_plot
+
+
+# In[46]:
+
+
+def get_client_info(client_id):
+    response = requests.get(f"https://fastapi-projet7-24875f0688c4.herokuapp.com/clients/{client_id}")
+    return response.json()
+
+# Créez une interface utilisateur Streamlit
+def main(data_shap):
+    st.title("Application tableau de bord interactif")
+    
+    image = Image.open('image_P7.png')
+    st.image(image, caption='Logo entreprise "prêt à dépenser"', use_column_width=True)
+    
+    # Saisie de l'identifiant client
+    client_id = st.selectbox("Sélectionnez l'identifiant client :", data_prob['SK_ID_CURR'].unique())
+    
+    # Vérifiez si l'identifiant client est saisi
+    if client_id:
+        # Appel de la fonction pour récupérer les informations du client
+        client_info = get_client_info(client_id)
+        
+        # Vérifiez si le client est trouvé
+        if "message" in client_info:
+            st.error("Client non trouvé")
+        else:
+            # Affichage des informations du client
+            st.subheader("Informations du client")
+            
+            # Formatage des informations en texte
+            info_text = ""
+            for key, value in client_info.items():
+                info_text += f"{key} {value}\n"
+            
+            # Affichage des informations formatées
+            st.text(info_text)
+            
+            # Récupération du score du client
+            score = client_info.get("Score du client:")
+                
+            # Comparaison de la probabilité prédite avec le seuil optimal
+            if score > optimal_threshold:
+                position = "Refusé"
+                position_color = "red"
+            else:
+                position = "Accepté"
+                position_color = "green"
+                    
+            # Affichage de la jauge
+            fig = jauge(score, optimal_threshold)
+            fig.update_layout(annotations=[
+                dict(
+                    x=0.5,
+                    y=0.45,
+                    text=position,
+                    showarrow=False,
+                    font=dict(color=position_color, size=24)
+                )
+            ])
+            st.plotly_chart(fig)
+            
+            st.subheader(f"Feature importance globale (à gauche) et locale du client {client_id} (à droite)")
+            
+            # Feature importance globale
+            fig, ax = plt.subplots()
+            summary_plot = shap.summary_plot(shap_values, data_shap_scaled, feature_names=data_shap.columns, show=False)
+            columns = st.columns(2)
+
+            with columns[0]:
+                st.pyplot(fig)
+                
+            # Réindexer le DataFrame avec des indices continus
+            data_shap = data_shap.reset_index(drop=True)
+
+            # Feature importance locale
+            client_data_shap = data_shap[data_shap['SK_ID_CURR'] == int(client_id)].index.item()
+            explanation_client = shap.Explanation(values=shap_values[1][client_data_shap], base_values=explainer.expected_value[1], feature_names=data_shap.columns)
+
+            # Création du waterfall plot pour le client spécifié
+            fig2, ax2 = plt.subplots()
+            waterfall_plot = shap.plots.waterfall(explanation_client, show=False)
+            with columns[1]:
+                st.pyplot(fig2)
+            
+            st.subheader("Distribution de la feature sélectionnée dans la liste")
+
+            # Sélection d'une feature pour le graphique de distribution
+            feature = st.selectbox("Sélectionnez une feature:", data_prob.columns)
+
+            # Récupération de la valeur de la feature pour le client sélectionné
+            client_data = data_prob[data_prob['SK_ID_CURR'] == int(client_id)]
+            if not client_data.empty:
+                client_value = client_data[feature].values[0]
+                # Affichage des graphiques de distribution
+                distri_features(data_prob, optimal_threshold, feature, client_value)
+            else:
+                st.warning("Aucune donnée disponible pour cet identifiant client")
+            
+            st.subheader("Analyse bi-variée entre 2 features sélectionnées (dégradé de couleur selon le score)")
+            
+            # Sélection de 2 features
+            selected_features = st.multiselect("Sélectionnez deux features:", data_prob.columns, default=[])
+            
+            # Vérification
+            if len(selected_features) != 2:
+                st.warning("Veuillez sélectionner exactement deux features.")
+            else:
+                feature1, feature2 = selected_features[0], selected_features[1]
+                # Affichage scatter plot
+                if not client_data.empty:
+                    bivarié_plot(feature1, feature2, data_prob, int(client_id))
+                else:
+                    st.warning("Aucune donnée disponible pour cet identifiant client")
+
+if __name__ == "__main__":
+    main(data_shap)
+
+
+# In[ ]:
+
+
+
+
---
+++ data_processing.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[76]:
+
+
+import streamlit as st
+import pandas as pd
+import pickle
+import gzip
+from sklearn.metrics import roc_curve, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+import imblearn
+
+# Importation du fichier clients (fichier original nettoyé)
+df = pd.read_csv('application_clean_sample.csv', sep=';')
+
+# Importation du fichier de données filtrées
+df_filtered = pd.read_csv('df_filtered_sample.csv', sep=';')
+data = df_filtered.copy()
+
+# On ouvre le fichier pickel contenant les informations relatives à notre modèle
+with gzip.open('modele_optimal.pickle.gz', 'rb') as file:
+    modele_save = pickle.load(file)
+
+# Accédez aux informations du modèle
+y_train = modele_save['y_train']
+y_test = modele_save['y_test']
+X_train = modele_save['X_train']
+X_test = modele_save['X_test']
+y_pred_prob_test = modele_save['y_pred_prob_test']
+y_pred_prob_train = modele_save['y_pred_prob_train']
+trained_model = modele_save['trained_model']
+
+# Ajout des probabilités aux données de test et d'entraînement
+X_train['Proba'] = y_pred_prob_train
+X_test['Proba'] = y_pred_prob_test
+
+# On concatène nos df
+data_prob = pd.concat([X_train, X_test])
+data_prob = data_prob[data_prob['SK_ID_CURR'].isin(df['SK_ID_CURR'])]
+
+# Calcul de la courbe ROC
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob_test)
+
+# Calcul de l'indice J de Youden pour chaque seuil
+j_scores = tpr + (1 - fpr) - 1
+
+# Trouver l'indice du seuil optimal qui minimise le FNR et maximise le TPR
+optimal_threshold_index = j_scores.argmax()
+
+# Seuil optimal
+optimal_threshold = thresholds[optimal_threshold_index]
+
+# Prédiction des classes avec le seuil optimal
+y_pred_optimal = (y_pred_prob_test >= optimal_threshold).astype(int)
+
+# Calcul des métriques de qualité de la classification avec le seuil optimal
+accuracy_optimal = accuracy_score(y_test, y_pred_optimal)
+precision_optimal = precision_score(y_test, y_pred_optimal)
+recall_optimal = recall_score(y_test, y_pred_optimal)
+f1_optimal = f1_score(y_test, y_pred_optimal)
+AUC_optimal = roc_auc_score(y_test, y_pred_prob_test)
+
+# On crée une fonction affichant la jauge de prédiction du seuil pour chaque client
+def jauge(value, optimal_threshold):
+    if value > optimal_threshold:
+        color='red'
+    else:
+        color='green'
+    fig = go.Figure(go.Indicator(
+        domain = {'x': [0, 1], 'y': [0, 1]},
+        value = value,
+        mode = "gauge+number",
+        title = {'text': "Probabilité prédite"},
+        gauge = {'axis': {'range': [None, 1]}, 
+                 'steps': [
+                     {'range': [0, 0.5], 'color': "lightgray"},
+                     {'range': [0.5, 1], 'color': "gray"}],
+                 'threshold': {'line' : {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': optimal_threshold},
+                 'bar' : {'color' : color}}))
+    return fig
+
+# On importe le fichier contenant les valeurs de shap
+with open('shap_values_sample.pickle', 'rb') as file:
+    shap_file_sample = pickle.load(file)
+
+shap_values = shap_file_sample['shap_values']
+data_shap_scaled = shap_file_sample['shap_scaled']
+explainer = shap_file_sample['explainer']
+data_shap = shap_file_sample['data_shap']
+
+# On crée une fonction qui affiche 2 graphiques de la distribution d'une feature sélectionnée
+# pour les 2 classes
+
+def distri_features(df, optimal_threshold, feature, client_value):
+    # Clients de la classe 1 (prêt non accordé) en utilisant le seuil
+    class_1_data = df[df['Proba'] >= optimal_threshold]
+    
+    # Clients de la classe 0 (prêt accordé) en utilisant le seuil
+    class_0_data = df[df['Proba'] < optimal_threshold]
+    
+    fig, axes = plt.subplots(1, 2, figsize=(16, 6))
+    
+    # Distribution de la feature pour la classe 0 (prêt accordé)
+    sns.histplot(class_0_data[feature], kde=True, color='green', ax=axes[0])
+    axes[0].axvline(client_value, color='blue', linestyle='dashed', linewidth=1)
+    axes[0].set_title(f"Distribution de la feature {feature} pour la classe 0 (prêt accordé)")
+    axes[0].set_xlabel(feature)
+    axes[0].set_ylabel('Count')
+
+    # Distribution de la feature pour la classe 1 (prêt non accordé)
+    sns.histplot(class_1_data[feature], kde=True, color='red', ax=axes[1])
+    axes[1].axvline(client_value, color='blue', linestyle='dashed', linewidth=1)
+    axes[1].set_title(f"Distribution de la feature {feature} pour la classe 1 (prêt non accordé)")
+    axes[1].set_xlabel(feature)
+    axes[1].set_ylabel('Count')
+    
+    plt.tight_layout()
+    
+    fig = plt.gcf()
+    st.pyplot(fig)
+    
+# On crée une fonction affichant un nuage de points entre 2 features sélectionnées
+
+def bivarié_plot(feature1, feature2, df, client_value):
+    # Scores des clients
+    score = df['Proba']
+    
+    # Données des deux fonctionnalités
+    data_features = df[[feature1, feature2]]
+    
+    # Positions des clients
+    client_positions = data_features[data_features.index == client_value]
+    
+    # Scatter plot
+    plt.figure(figsize=(10, 6))
+    scatter = plt.scatter(data_features[feature1], data_features[feature2], c=score, cmap='coolwarm')
+    plt.colorbar(scatter, label='Scores')
+    plt.scatter(client_positions[feature1], client_positions[feature2], color='black', marker='*', s=200, label='Client')
+    plt.xlabel(feature1)
+    plt.ylabel(feature2)
+    plt.title('Analyse bi-variée entre {} et {} pour le client {}'.format(feature1, feature2, client_value))
+    plt.legend(loc='upper right')
+    
+    fig = plt.gcf()
+    st.pyplot(fig)
+
